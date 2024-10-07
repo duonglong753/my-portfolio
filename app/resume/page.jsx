c210883361a97ac5894798d942ab8b63b272f293
Hung Long <duonglong753@gmail.com>
2024-10-07 15:53:25 +0700

    add resume page (2/3 process)

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,268 @@
+"use-client";
+import { Description } from "@radix-ui/react-dialog";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+const about = {
+  title: "About me",
+  decription:
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas, repudiandae, expedita odio sunt minus dolores modi ipsum rem quam vel asperiores culpa pariatur, exercitationem neque aut autem iure dolor velit.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Duong Hung Long",
+    },
+    {
+      fieldName: "Phone ",
+      fieldValue: "(+84)932749306",
+    },
+    {
+      fieldName: "education",
+      fieldValue: "2+ years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Viet Nam",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "duonglong753@gmail.com",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "longdh1301",
+    },
+    {
+      fieldName: "Freelancer",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Chinese",
+    },
+  ],
+};
+const experiences = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi facere dolor, placeat voluptatem, architecto sequi hic a maxime alias in tenetur eos? Nostrum minus dicta voluptate cum at, consectetur laborum?",
+  items: [
+    {
+      company: "Tech solutions Inc.",
+      position: "Full stack developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "BeeTech Solutions Inc.",
+      position: "Software Developer Intern",
+      duration: "Summer 2021",
+    },
+    {
+      company: "E-commerce Startup",
+      position: "Freelance Web Developer",
+      duration: "2020-2021",
+    },
+    {
+      company: "Tech Academy",
+      position: "Teaching Assistant",
+      duration: "2019-2021",
+    },
+    {
+      company: "Digital Agency",
+      position: "UI/UX Designer",
+      duration: "2018-2019",
+    },
+    {
+      company: "Software Development Firm",
+      position: "Junior Developer",
+      duration: "2017-2028",
+    },
+  ],
+};
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi facere dolor, placeat voluptatem, architecto sequi hic a maxime alias in tenetur eos? Nostrum minus dicta voluptate cum at, consectetur laborum?",
+  items: [
+    {
+      institution: "Online Course Platform",
+      degree: "Full Stack Web development Bootcamp",
+      duration: "2023",
+    },
+    {
+      institution: "Codecademy",
+      degree: "Front-end Track",
+      duration: "2022",
+    },
+    {
+      institution: "Online Course",
+      degree: "Programming Course",
+      duration: "2020-2021",
+    },
+    {
+      institution: "Tech Institute",
+      degree: "Certified Web Developer",
+      duration: "2019",
+    },
+    {
+      institution: "Information Technology School",
+      degree: "Diploma in Information Technology",
+      duration: "2018-2023",
+    },
+    {
+      institution: "Community Colleague",
+      degree: "Associate Degree in Computer Science",
+      duration: "2018-2022",
+    },
+  ],
+};
+const skills = {
+  title: "My skills",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit fuga corporis inventore iure minima, ipsam neque, consequatur at pariatur recusandae perferendis iste quo atque provident libero iusto. Quam, provident neque.",
+  skilllist: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "reactjs",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "nextjs",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwindcss",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "nodejs",
+    },
+    {
+      icon: <FaFigma />,
+      name: "figma",
+    },
+  ],
+};
 const Resume = () => {
-  return <div>Resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="contianer mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col">
+                <h3 className="text-4xl font-bold">{experiences.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experiences.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul>
+                    {experiences.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul>
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
