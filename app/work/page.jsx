8ef0e9bcb5a01b70a9b3b809a731a536ce7d5258
Hung Long <duonglong753@gmail.com>
2024-10-08 12:05:19 +0700

    work page : project info (1/2 process)

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -36,9 +36,84 @@ const projects = [
     live: "",
     github: "",
   },
+  {
+    num: "03",
+    category: "Back-end",
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis natus facilis perspiciatis id. Itaque officiis in omnis veniam magni? Pariatur maiores explicabo, sed id ut eligendi laborum consequuntur hic sequi!",
+    stack: [{ name: "NextJs" }, { name: "NodeJs" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
 ];
 const Work = () => {
-  return <div>page</div>;
-};
+  const [project, setProject] = useState(projects[0]);
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8l leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
 
-export default Work;
+              {/*stack*/}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}{" "}
+                      {/*remove the last comma*/}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4">
+                {/*live project btn*/}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* github project btn*/}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Githhub repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+            <div className="w-full xl:w-[50%]"></div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
