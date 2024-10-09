6294f30473e21429176c0c562a8c79bd0f0494a6
Hung Long <duonglong753@gmail.com>
2024-10-09 14:56:27 +0700

    bo sung css

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -195,13 +195,13 @@ const Resume = () => {
           </TabsList>
           <div className="min-h-[70vh] w-full">
             <TabsContent value="experience" className="w-full">
-              <div className="flex flex-col">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{experiences.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                   {experiences.description}
                 </p>
                 <ScrollArea className="h-[400px]">
-                  <ul>
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {experiences.items.map((item, index) => {
                       return (
                         <li
