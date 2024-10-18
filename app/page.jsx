67b457c612b05e1857902988c866b1e284791d28
Hung Long <duonglong753@gmail.com>
2024-10-18 16:15:31 +0700

    add comments & fix css

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,8 @@ const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-coll xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/*TEXT*/}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
@@ -34,6 +35,7 @@ const Home = () => {
               </div>
             </div>
           </div>
+          {/*photo*/}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
